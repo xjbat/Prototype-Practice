adfd6188dad5250ca3a8486819899fff7be51aad
xjbat <xjbatology@gmail.com>
2026-06-04 12:05:22 -0500

    Added level system functionality

--- prototyping-project/scripts/global.gd
+++ prototyping-project/scripts/global.gd
@@ -7,3 +7,19 @@ signal grow
 signal shrink
 signal activate_platform
 signal deactivate_platform
+@onready var root = get_node("../root")   
+@export var scenes = []
+var scene_index = 0
+var current_scene
+
+func _ready() -> void:
+	current_scene = scenes[scene_index].instantiate()
+	root.add_child(current_scene)
+	change_scene()
+
+func change_scene():
+	current_scene.queue_free()
+	scene_index += 1
+	current_scene = scenes[scene_index].instantiate()
+	root.add_child(current_scene)	
+	
