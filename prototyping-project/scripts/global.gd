b5bd0984ed6474b6716e35f3b2979cfdc9944013
xjbat <xjbatology@gmail.com>
2026-06-06 11:16:50 -0500

    Added restart functionality
    Made a restart button sprite, a restart button as the child of the Global game object, positioned it to be in the right place, and set it to call reset_scene() in the Global script. Mild refactor of Global level system.

--- prototyping-project/scripts/global.gd
+++ prototyping-project/scripts/global.gd
@@ -14,21 +14,30 @@ signal deactivate_platform
 @onready var root = get_node("../root")   
 @export var scenes = []
 #hacky fix so level one loads first
-var scene_index = -1
+var scene_index = 0
 var current_scene
 
 #changed from root.add_child() to call_deferred("add_child") to get the output to shut up
 func _ready() -> void:
 	current_scene = scenes[scene_index].instantiate()
 	root.call_deferred("add_child", current_scene)
-	change_scene()
+	reset_scene()
 
 func change_scene():
 	scene_index += 1
 	if (scene_index >= len(scenes)):
 		push_warning("No next scene available")
+		scene_index -= 1
 	else:
 		current_scene.queue_free()
 		current_scene = scenes[scene_index].instantiate()
 		root.call_deferred("add_child", current_scene)
-	
+		
+func reset_scene():
+	scene_index -= 1
+	change_scene()
+
+
+
+func _on_restart_button_pressed() -> void:
+	reset_scene()
