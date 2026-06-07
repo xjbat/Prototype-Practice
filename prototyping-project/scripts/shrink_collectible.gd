06ca6e44e4e3a5fea9e712a676cf3badef1ea149
xjbat <xjbatology@gmail.com>
2026-06-07 14:23:12 -0500

    Mild polish
    You can't pick up objects twice, fixed player movement values and reworked level 5

--- prototyping-project/scripts/shrink_collectible.gd
+++ prototyping-project/scripts/shrink_collectible.gd
@@ -3,7 +3,11 @@ extends Area2D
 @export var workout_sound: AudioStreamMP3
 
 func _on_body_entered(body: Node2D) -> void:
-	if (Global.playerState != Global.PlayerStates.THIN and body is CharacterBody2D):
-		Global.shrink.emit()
+	if (body is CharacterBody2D):
 		Global.play_audio(workout_sound, global_position, 1)
 		queue_free()
+		if (Global.playerState != Global.PlayerStates.THIN): 
+			Global.shrink.emit()
+		else:
+			Global.reset_scene()
+		
